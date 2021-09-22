dfdbc02a9cc7b89a1985538fd724bc744be38fdf
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-09-22 13:14:37 -0500

    Started working on home page and Home tab on navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Container
+} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component{
+  state = {
+    account: []
+  }
+  componentDidMount(){
+    this.getAccounts();
+  }
+
+  async getAccounts(){
+    try {
+      let response = await axios.get("http://127.0.0.1:8000/api/accounts/employees/")
+      this.setState({
+        account : response.data
+      })
+      console.log(response.data)
+    }
+      catch(except){
+        console.log("API Error")
+      }
+  }
+
+  render(){
+    return(
+      <div className="App" style={{
+        backgroundColor: 'beige',
+        backgroundSize: 'cover',
+        height: '100vh'
+      }}>
+        <Router>
+          <div className="navbar">
+            <Navbar id='navbar' fixed='top' bg='black' variant='dark'>
+            <Navbar.Brand href='' id="Logo">Welcome to Sewing By Suzy!</Navbar.Brand>
+            <Nav.Link id="NavLinks" href='/home'> Home</Nav.Link>
+            <Nav className="ml-auto">
+              <SearchBar />
+            </Nav>
+            </Navbar>
+          </div>
+          <Switch>
+            <Route path='/home' exact component={Home}></Route>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+};
 
 export default App;
+
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomeStyle.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <div class="container">
+                <div class="row">
+                    <h1 id="Homepageh1" class="text-center"> Meet Our Team!</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
