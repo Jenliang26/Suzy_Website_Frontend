c4dddf1bfd82bf6cf38d4c258133f8f3ceb9e4d1
Jenny Kay <jennykay@Jennys-MBP.attlocal.net>
2021-10-04 16:42:44 -0500

    A customer is now able to leave a review as an anonymous person to the business

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,33 +1,49 @@
-// import axios from 'axios';
-// import "./Reviews.css";
-// import { Component } from 'react';
-// import "./GetReview";
+import axios from 'axios';
+import "./Reviews.css";
+import { Component } from 'react';
+import "./GetReview";
+import Button from 'react-bootstrap/button';
 
 
-// class Reviews extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {}
-//     }
+class Reviews extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            review: '',
+            rating: '',
+            name: 'Anonymous',
+            date: ''
+        }
+        this.postreview = this.postreview.bind(this)
+    }
     
-//     componentDidMount () {
-//         this.GetReviews ()
-//     }
+    async postreview() {
+        let date = new Date()
+        let datestring = ((date.getFullYear() +  "-" + (date.getMonth()+1) + "-" + date.getDate()))
+        let comment = {
+            name: this.state.name,
+            date: datestring,
+            number_rating: this.state.rating,
+            comment: this.state.review
+        }
+        console.log(comment)
+        let response = await axios.post('http://127.0.0.1:8000/api/reviews/', comment)
+    }
 
-//     async GetReviews() {
-//         let response = await axios.get('http://127.0.0.1:8000/api/reviews/'),
-//         reviews = response.data
-//         console.log(reviews.data)
-//     }
-    
+    render() {
+        return (
+            <div className="text-center">
+                <h3>Name</h3>
+                <input onChange={(e) => this.setState({name: e.target.value})} value={this.state.name} name="name" id="name" type="text" />
+                <h3>Rating</h3>
+                <input onChange={(e) => this.setState({rating: e.target.value})} value={this.state.rating} name="rating" id="rating" type="text" />
+                <h3>Review</h3>
+                <textarea rows="5" cols="80" onChange={(e) => this.setState({review: e.target.value})} value={this.state.review} name="review" id="review" type="text" />
+                <br />
+                <Button onClick={this.postreview}>Add Review</Button>
+            </div>
+        );
+    }
+}
 
-//     render() {
-//         return (
-//             <div>
-//                 <GetReview  Reviews = {this.state.reviews} />
-//             </div>
-//         );
-//     }
-// }
-
-// export default Reviews;
+export default Reviews;
